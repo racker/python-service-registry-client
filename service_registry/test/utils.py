195a106305a9f861b3a202644904662b48685bd3
Tomaz Muraus <tomaz@tomaz.me>
2013-02-16 16:24:13 +0100

    Fix mock http api runner (backport fixed from Kami/python-yubico-client).

--- service_registry/test/utils.py
+++ service_registry/test/utils.py
@@ -24,7 +24,7 @@ from os.path import join as pjoin
 
 # From https://github.com/Kami/python-yubico-client/blob/master/tests/utils.py
 
-def waitForStartUp(process, pid, address, timeout=10):
+def waitForStartUp(process, address, timeout=10):
     # connect to it, with a timeout in case something went wrong
     start = time.time()
     while time.time() < start + timeout:
@@ -38,57 +38,18 @@ def waitForStartUp(process, pid, address, timeout=10):
         # see if process is still alive
         process.poll()
 
-        if pid and process.returncode is None:
-            os.kill(pid, signal.SIGKILL)
+        if process and process.returncode is None:
+            process.terminate()
         raise RuntimeError("Couldn't connect to server; aborting test")
 
 
 class ProcessRunner(object):
     def setUp(self, *args, **kwargs):
-        # clean up old.
-        p = self.getPid()
-        if p is not None:
-            try:
-                # remember, process may already be dead.
-                os.kill(p, 9)
-                time.sleep(0.01)
-            except:
-                pass
+        pass
 
     def tearDown(self, *args, **kwargs):
-        spid = self.getPid()
-        if spid:
-            max_wait = 1
-            os.kill(spid, signal.SIGTERM)
-            slept = 0
-            while (slept < max_wait):
-                time.sleep(0.5)
-                if not self.isAlive(spid):
-                    if os.path.exists(self.pid_fname):
-                        os.unlink(self.pid_fname)
-                    break
-                slept += 0.5
-            if (slept > max_wait and self.isAlive(spid)):
-                os.kill(spid, signal.SIGKILL)
-                if os.path.exists(self.pid_fname):
-                    os.unlink(self.pid_fname)
-                raise Exception('Server did not shut down correctly')
-        else:
-            print 'Unable to locate pid file (%s)!' % self.pid_fname
-
-    def isAlive(self, pid):
-        try:
-            os.kill(pid, 0)
-            return 1
-        except OSError, err:
-            return err.errno == errno.EPERM
-
-    def getPid(self):
         if self.process:
-            return self.process.pid
-        elif os.path.exists(self.pid_fname):
-            return int(open(self.pid_fname, 'r').read())
-        return None
+            self.process.terminate()
 
 
 class MockAPIServerRunner(ProcessRunner):
@@ -106,16 +67,15 @@ class MockAPIServerRunner(ProcessRunner):
         script = pjoin(os.path.dirname(__file__), 'mock_http_server.py')
 
         with open(self.log_path, 'a+') as log_fp:
-            port_arg = '%s --port=%s' % (script, self.port)
             fixtures_dir_arg = \
                 '--fixtures-dir=service_registry/test/fixtures/response/'
-            args = '%s %s' % (port_arg, fixtures_dir_arg)
+            port_arg = '%s --port=%s' % (script, self.port)
+            args = [script, port_arg, fixtures_dir_arg]
             self.process = subprocess.Popen(args,
-                                            shell=True,
+                                            shell=False,
                                             cwd=self.base_dir,
                                             stdout=log_fp,
                                             stderr=log_fp)
             waitForStartUp(self.process,
-                           self.getPid(),
                            ('127.0.0.1', self.port), 10)
         atexit.register(self.tearDown)
